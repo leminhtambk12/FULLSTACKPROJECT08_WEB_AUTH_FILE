254fbef4d167d0c8c4c6732911620621262e650e
Tam le minh <leminhtambk12@gmail.com>
2021-03-14 14:52:03 +0700

    Set up passport UserModel connect db and initialize routes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+//Initialize App
 const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan("dev"));
-
-app.get("/", (req, res) => res.send("hello"));
+//Use passport middleware
+app.use(passport.initialize());
+//Bring passport Strategy
+require("./config/passport")(passport);
+//Bring Database from MongoDB
+const db = require("./config/keys").mongoURI;
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Server has been connected to ${db}`))
+    .catch((err) => console.log(`Unable to connect database ${err}`));
+//Routes
+app.use("/api/users", require("./Routes/users"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:admin123@vueexpress01.5ngbp.mongodb.net/Login_Logout?retryWrites=true&w=majority",
+    secret: "yourSecret",
+};
