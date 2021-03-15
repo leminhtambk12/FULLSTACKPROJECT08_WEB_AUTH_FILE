dc8643522df2c50fbef404d0c0643f12d9c0670a
Tam le minh <leminhtambk12@gmail.com>
2021-03-15 12:18:09 +0700

    Design Navbar for router by Vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> </v-app-bar>
-
+    <Navbar />
     <v-main>
-      <v-container> abc</v-container>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
   name: "App",
 
-  components: {},
+  components: { Navbar },
 
   data() {
     return {};
   },
 };
 </script>
+<style>
+/* Reset Css  */
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="navbar">
+    <v-app-bar app color="primary" class="white--text" flat dark>
+      <v-container fluid>
+        <div class="titleApp">
+          <router-link to="/" class="white--text headline">
+            MEVN-Youtube_Ideas
+          </router-link>
+        </div>
+        <div style="float:right;">
+          <v-btn color="primary" depressed>
+            <router-link class="white--text" to="/">Home</router-link>
+          </v-btn>
+          <v-btn color="primary" depressed>
+            <router-link class="white--text" to="/about">About</router-link>
+          </v-btn>
+          <v-btn color="primary" depressed>
+            <router-link class="white--text" to="/login">Login</router-link>
+          </v-btn>
+          <v-btn color="primary" depressed>
+            <router-link class="white--text" to="/register">
+              Register
+            </router-link>
+          </v-btn>
+          <v-btn color="primary" depressed>
+            <router-link class="white--text" to="/profile">Profile</router-link>
+          </v-btn>
+          <v-btn color="primary" depressed>
+            <router-link class="white--text" to="/logout">Logout</router-link>
+          </v-btn>
+        </div>
+      </v-container>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.titleApp {
+  display: inline-block;
+  width: 30%;
+}
+</style>
